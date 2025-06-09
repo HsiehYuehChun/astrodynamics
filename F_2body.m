a09a3c27fd33048c3cdd5fdd8435407e1c29ea8e
Hsieh Yueh-Chen <wilson5711704@gmail.com>
2025-06-09 17:22:18 +0800

    Update F_2body.m

--- F_2body.m
+++ F_2body.m
@@ -2,7 +2,7 @@ function Rdot = F_2body(t, R)
 %二體問題微分方程表示式
 % t: time (not used in 2-body model, but required for ODE solver compatibility)
 
-mu = 398600;  % km^3/s^2
+mu = 398600.4418;  % km^3/s^2
 
 pos = R(1:3);       % [x; y; z]
 vel = R(4:6);       % [vx; vy; vz]
